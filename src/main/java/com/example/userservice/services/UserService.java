7d1f813fa7e1b03c0c1718be428f3deb5b112901
AshwanikDubey <ashwanidubey.work@gmail.com>
2025-03-21 22:38:10 +0530

    Creating user repo

--- src/main/java/com/example/userservice/services/UserService.java
+++ src/main/java/com/example/userservice/services/UserService.java
@@ -1,10 +1,23 @@
 package com.example.userservice.services;
 
 import com.example.userservice.models.User;
+import com.example.userservice.repos.UserRepo;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
+    private UserRepo userRepo;
+
+    private UserService (UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
 
     public User signUp(String name , String email , String password){
+        User user = new User();
+        user.setEmail(email);
+        user.setUsername(name);
+        user.setHashedPassword(password);
 
+        return userRepo.save(user);
     }
 }
